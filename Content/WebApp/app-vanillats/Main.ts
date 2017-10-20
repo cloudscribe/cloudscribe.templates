750e9ef72a24b17488bcf063474f9ad53a063c17
Joe Audette <joe.audette@gmail.com>
2017-10-20 15:59:26 -0400

    add files for client setup

---
+++ Content/WebApp/app-vanillats/Main.ts
@@ -0,0 +1,13 @@
+﻿class Greeter {
+    constructor(public greeting: string) { }
+    greet() {
+        return "<h1>" + this.greeting + "</h1>";
+    }
+};
+
+var greeter = new Greeter("Hello, world from typescript!");
+var ele = document.getElementById("greeter");
+if (ele) {
+    ele.innerHTML = greeter.greet();
+}
+
