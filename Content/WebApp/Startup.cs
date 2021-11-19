e0f7b3f5c454e73b2f50f6784563b7726e3ece00
jimkerslake <jimkerslake@esdm.co.uk>
2021-11-19 14:44:20 +0000

    memorycache

--- Content/WebApp/Startup.cs
+++ Content/WebApp/Startup.cs
@@ -42,6 +42,8 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+
             //// **** VERY IMPORTANT *****
             // This is a custom extension method in Config/DataProtection.cs
             // These settings require your review to correctly configur data protection for your environment
